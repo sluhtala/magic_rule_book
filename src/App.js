bc0e3540c21fc9d15904e2224e9b40b9a4b748d4
Sasu Luhtala <sluhtala@me.com>
2021-06-22 23:26:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { getRules } from './services/dataService';
+import parser from './services/parser';
+import TableOfContents from './contents/Contents';
+import { Container, Grid, Loader } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import MainContent from './main';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	const [rules, setRules] = useState([]);
+	const [contents, setContents] = useState([]);
+	const [glossary, setGlossary] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [selectedSection, setSelectedSection] = useState({});
+	const [lastSection, setLastSection] = useState(null);
+	const [rulesArray, setRulesArray] = useState([]);
+
+	useEffect(()=>{
+		getRules()
+		.then((data)=>{
+			const lines = data.split('\n').map((line)=>line.trim());
+			setContents(parser.getContent(lines))
+			setRules(parser.getRules(lines));
+			setGlossary(parser.getGlossary(lines));
+			setRulesArray(parser.createRulesArray);
+			setIsLoading(false);
+		})
+		.catch((e)=>{
+			console.error(e.message);
+		})
+	},[]);
+
+	useEffect(()=>{
+		if (rules.length === 0)
+			return;
+		let last = 0;
+		rules[rules.length - 1].content.forEach((s)=>{
+			last = s.number;
+		})
+		setLastSection(last)
+	},[rules])
+
+	return (
+		<Container>
+				<Grid columns={2} divided stackable>
+					<Grid.Row>
+						<Grid.Column width={4}>
+							<TableOfContents
+								contents={contents}
+								selectedSection={selectedSection}
+								setSelectedSection={setSelectedSection}/>
+						</Grid.Column>
+						<Grid.Column width={12}>
+								{isLoading ? '' :
+								<MainContent 
+									selectedSection={selectedSection}
+									setSelectedSection={setSelectedSection}
+									rules={rules}
+									glossary={glossary}
+									lastSection={lastSection}
+									rulesArray={rulesArray}
+									/>
+								}
+								<Loader active={isLoading} />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+		</Container>
   );
 }
 
