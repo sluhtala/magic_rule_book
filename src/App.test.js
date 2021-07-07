b3b251acf5655f3cbbfce7d783791f2f13c59737
Sasu Luhtala <sluhtala@me.com>
2021-07-07 13:47:10 +0300

    added a new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import Contents from "./Contents";
 
 test("if renders the page header", () => {
     render(<App />);
@@ -8,4 +9,25 @@ test("if renders the page header", () => {
         "An unofficial Magic: The Gathering -rulebook"
     );
     expect(text).toBeInTheDocument();
+});
+
+test("if rules can be rendered", () => {
+    const rbook = {
+        rules: [{ id: 1, text: "101.1 test rule", headerNum: 1, subNum: 101 }],
+        headers: [{ id: 2, text: "1 header" }],
+        subHeaders: [{ id: 3, text: "101 sub" }],
+    };
+    render(
+        <>
+            {rbook.rules && rbook.headers && rbook.subHeaders && (
+                <Contents rBook={rbook} />
+            )}
+        </>
+    );
+    const header = screen.getByText("1 header");
+    const sub = screen.getByText("101 sub");
+    const rule = screen.getByText("test rule");
+    expect(header).toBeInTheDocument();
+    expect(sub).toBeInTheDocument();
+    expect(rule).toBeInTheDocument();
 });
